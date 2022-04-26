afb00e4ea4a5a2b390fa724140fac6d07859fe9f
Your Name <you@example.com>
2022-04-26 17:08:14 +0800

    init

--- training/src/make.sh
+++ training/src/make.sh
@@ -1,7 +1,6 @@
 # Replace by your own python 
 python_header=${CONDA_PREFIX}/include/python3.8
 # The dir that you install tqdm, cnpy, tinyply 
-DEPENDENCY="/usr/local/lib"
 CUDA_DIR="/usr/local/cuda"
 
 
@@ -19,11 +18,8 @@ fi
 # rm -r $BASEDIR/build 
 # python setup.py build --build-lib $LIBDIR
 # mv $LIBDIR/CUDA_EXT.*.so $LIBDIR/CUDA_EXT.so 
-if [$? == 0];then
-    echo "Build CUDA_EXT successfully!"
-else
-    echo "[failed] Build CUDA_EXT!"
-fi
+# echo "Build CUDA_EXT successfully!"
+
 
 nvcc -std=c++11 --shared --compiler-options "-fpic -shared" \
 -I $INCLUDE_DIR -c -o $LIBDIR/libcuda_func.so cuda_func.cu
@@ -36,28 +32,19 @@ preparedata.cpp \
 -I $CUDA_DIR/include \
 -L $CUDA_DIR/lib64 \
 -L $LIBDIR -lcuda_func \
--L /usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc \
--L $DEPENDENCY -ltinyply -lcnpy \
+-L /usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -ltinyply -lcnpy \
 -o $LIBDIR/preparedata.so \
 -lcudart -lcuda
+echo "Build preparedata successfully!"
 
-if [$? == 0];then
-    echo "Build preparedata successfully!"
-else
-    echo "[failed] Build preparedata!"
-fi
 
 
 g++ -std=c++11 -fPIC -shared \
 warp.cpp \
 -o $LIBDIR/compute_grid.so  \
 -I $python_header
+echo "Build compute_grid successfully!"
 
-if [$? == 0];then
-    echo "Build compute_grid successfully!"
-else
-    echo "[failed] Build compute_grid!"
-fi
 
 
 g++ -std=c++11 -fPIC -shared \
@@ -67,13 +54,7 @@ group_tiles.cpp \
 -I $CUDA_DIR/include \
 -L $CUDA_DIR/lib64 \
 -L $LIBDIR -lcuda_func \
--L /usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc \
--L $DEPENDENCY -ltinyply -lcnpy \
+-L /usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -ltinyply -lcnpy \
 -o $LIBDIR/group_tiles.so \
 -lcudart -lcuda
-
-if [$? == 0];then
-    echo "Build group_tiles successfully!"
-else
-    echo "[failed] Build group_tiles!"
-fi
+echo "Build group_tiles successfully!"
